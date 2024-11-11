8f464030342d4e0eccbfc699c61a12372aa5962d
mohbens <mohbenselama@gmail.com>
2024-11-11 11:35:44 +0100

    adding the SampleProducts and show them in the first page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,25 @@
 import "./App.css";
+import { sampleProducts } from "./data";
 
 function App() {
 	return (
 		<div>
 			<header>TS Amazon</header>
+			<main>
+				<ul>
+					{sampleProducts.map((product) => (
+						<li key={product.slug}>
+							<img
+								src={product.image}
+								alt={product.name}
+								className="product-image "
+							/>
+							<h2>{product.name}</h2>
+							<p>{product.price}€</p>
+						</li>
+					))}
+				</ul>
+			</main>
 			<footer>All right reserved</footer>
 		</div>
 	);
